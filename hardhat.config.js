d44a091c724274cf04a1829af2cfe2c3c977eb63
Shantanu Bharadwaj <theshantanubharadwaj@gmail.com>
2023-09-20 08:56:10 +0530

    Updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,10 +9,4 @@ const ROPSTEN_PRIVATE_KEY = ""; //PUT YOUR PRIVATE KEY HERE
 
 module.exports = {
   solidity: "0.8.0",
-  // networks: {
-  //   ropsten: {
-  //     url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-  //     accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
-  //   },
-  // },
 };
